9f20c699e6ab3da068802ba7acb1aa38824012dc
Diallo Souleyman <diallisouleyman78@gmail.com>
2026-01-20 11:52:00 +0100

    data transformation

---
+++ src/components/data_transformation.py
@@ -0,0 +1,99 @@
+import sys
+from dataclasses import dataclass
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer # for applying different transformations to different columns create pipelines
+from sklearn.impute import SimpleImputer # for handling missing values
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from src.exception import CustomException
+from src.logger import logging
+import os
+from src.utils import save_object
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path = os.path.join('artifacts', 'preprocessor.pkl') # path to save the preprocessor object
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config = DataTransformationConfig() # create an instance of DataTransformationConfig
+
+    def get_data_transformer_object(self):   
+
+        '''
+        Responsible for data transformation
+        1. Numerical columns: imputation (median), scaling (StandardScaler)
+        2. Categorical columns: imputation (most frequent), encoding (OneHotEncoder), scaling (StandardScaler)
+        '''
+        try:
+            numeric_columns = ['reading score', 'writing score']
+            categorical_columns = ['gender', 'race/ethnicity', 'parental level of education',
+                                   'lunch', 'test preparation course']
+            
+            #numeric pipeline
+            num_pipeline = Pipeline(steps=[
+                ('imputer', SimpleImputer(strategy='median')),
+                ('scaler', StandardScaler(with_mean=False)) # scaling without centering to handle sparse matrix issue
+            ])
+
+            #categorical pipeline
+            cat_pipeline = Pipeline(steps=[
+                ('imputer', SimpleImputer(strategy='most_frequent')), # replace missing values with most frequent value
+                ('one_hot_encoder', OneHotEncoder()), # convert categorical variables to numerical variables
+                ('scaler', StandardScaler(with_mean=False)) # scale the data
+            ])
+            logging.info("Numerical and categorical pipeline created/ encoding completed")
+
+            preprocessor = ColumnTransformer([
+                ('num_pipeline', num_pipeline, numeric_columns), # pipeline and columns
+                ('cat_pipeline', cat_pipeline, categorical_columns) # pipeline and columns
+            ])
+
+
+            return preprocessor
+        
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+    def initiate_data_transformation(self, train_path, test_path):    
+        try:
+            
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+            logging.info("Read train and test data completed")
+            logging.info("Obtaining preprocessor object")
+
+            preprocessing_obj = self.get_data_transformer_object()
+
+            target_column_name = 'math score'
+            numeric_columns = ['reading score', 'writing score']
+
+            input_feature_train_df = train_df.drop(columns =[target_column_name], axis=1)
+            target_feature_train_df = train_df[target_column_name]
+
+            input_feature_test_df = test_df.drop(columns =[target_column_name], axis=1)
+            target_feature_test_df = test_df[target_column_name]
+            logging.info("Applying preprocessing object on training and testing dataframe")
+
+            input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df) # fit and transform on training data
+            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df) # only transform on testing data 
+
+            train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)] # concatenate numpy arrays
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+            logging.info("Concatenating input and target features array")
+
+            save_object(
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+            )
+
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path,
+            )
+
+
+        except Exception as e:
+            raise CustomException(e, sys)
--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,17 @@
+import os
 import sys
+import pandas as pd
+import numpy as np
+from src.exception import CustomException
+import dill
 
-def error_message_details(error, error_detail:sys):
-    _, _, exc_tb = error_detail.exc_info()#in which file the error occured
-    file_name = exc_tb.tb_frame.f_code.co_filename#line number where the error occured
-    error_message = "Error Occured in python script name [{0}] and line number [{1}] error message [{2}]".format(
-        file_name,exc_tb.tb_lineno,str(error)
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True) # create directory if it does not exist
 
-    return error_message    
-    )
-
-class CustomException(Exception):
-    def __init__(self,error_message, error_detail:sys):
-        super().__init__(error_message)
-        self.error_message = error_message_details(error_message, error_detail)
-
-    def __str__(self):
-        return self.error_message    
+        with open(file_path, 'wb') as file_obj:
+            dill.dump(obj, file_obj) 
+    except Exception as e: 
+        raise CustomException(e, sys)
+    #saving preprocessor pickle in a file disk
